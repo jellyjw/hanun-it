8ddf17f8a34880aeb5481535afbb6a757704bb44
jellyjw <wldnzzz@naver.com>
2025-05-27 13:16:35 +0900

    Initial commit

---
+++ src/lib/axios.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
+
+// axios 인스턴스 생성
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // 토큰이 필요한 경우 여기서 헤더에 추가
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    // 에러 처리 로직
+    if (error.response?.status === 401) {
+      // 인증 에러 처리
+      localStorage.removeItem("token");
+    }
+    return Promise.reject(error);
+  }
+);
+
+// HTTP 메서드 래퍼 함수들
+export const http = {
+  get: <T>(url: string, config?: AxiosRequestConfig) =>
+    axiosInstance.get<T>(url, config).then((response) => response.data),
+
+  post: <T>(url: string, data?: T, config?: AxiosRequestConfig) =>
+    axiosInstance.post<T>(url, data, config).then((response) => response.data),
+
+  put: <T>(url: string, data?: T, config?: AxiosRequestConfig) =>
+    axiosInstance.put<T>(url, data, config).then((response) => response.data),
+
+  delete: <T>(url: string, config?: AxiosRequestConfig) =>
+    axiosInstance.delete<T>(url, config).then((response) => response.data),
+
+  patch: <T>(url: string, data?: T, config?: AxiosRequestConfig) =>
+    axiosInstance.patch<T>(url, data, config).then((response) => response.data),
+};
+
+export default axiosInstance;
