c33b0a50a7ef02e9b53a80b992991774b2d782de
jellyjw <wldnzzz@naver.com>
2025-05-31 22:56:48 +0900

    feature: 소셜로그인 기능 추가

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  const next = searchParams.get("redirect") ?? "/articles";
+
+  if (code) {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+
+      if (isLocalEnv) {
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
+  }
+
+  // 오류가 발생한 경우 로그인 페이지로 리다이렉트
+  return NextResponse.redirect(`${origin}/auth/login?error=auth_failed`);
+}
