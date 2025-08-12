8185e3fc5834034c6b247c28a48a09dc3fc373ad
jellyjw <wldnzzz@naver.com>
2025-08-12 20:23:04 +0900

    fix: ads.txt 경로 변경

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { updateSession } from "@/utils/supabase/middleware";
-import { type NextRequest } from "next/server";
+import { updateSession } from '@/utils/supabase/middleware';
+import { type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
   // update user's auth session
@@ -13,8 +13,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - robots.txt, ads.txt, sitemap.xml and other text/xml files
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next/static|_next/image|favicon.ico|robots\\.txt|ads\\.txt|sitemap\\.xml|.*\\.(?:svg|png|jpg|jpeg|gif|webp|txt|xml)$).*)',
   ],
 };
